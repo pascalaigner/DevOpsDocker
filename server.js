db36f84bfd205bb6e6b2000e5c228eea4d66f705
Pascal Aigner <pascal.aigner97@outlook.com>
2024-05-26 00:43:29 +0200

    adjusted display text to test automatic deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello! This is Pascal!');
+	res.send('Hello! This is Pascal! This version was deployed automatically after git commit');
 });
 
 app.listen(PORT, HOST);
