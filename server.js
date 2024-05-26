a573635e7a8653f79fcdc09a34d24f789cd15ae2
Pascal Aigner <pascal.aigner97@outlook.com>
2024-05-26 14:28:07 +0200

    changed display text

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello! This is Pascal! This version was deployed automatically after git commit');
+	res.send('Hello! This is Pascal!');
 });
 
 app.listen(PORT, HOST);
