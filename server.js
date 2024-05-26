f3bf46285682c15de6aa991f15cf917e73aa1bc5
Pascal Aigner <pascal.aigner97@outlook.com>
2024-05-26 14:38:19 +0200

    added new text

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello! This is Pascal!');
+	res.send('Hello! This is Pascal! New text after commit');
 });
 
 app.listen(PORT, HOST);
